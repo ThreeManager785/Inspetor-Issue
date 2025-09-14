3b807d926475b9ef678bdc9d9004a38f956ed84d
ThreeManager785 <leimeichun50@gmail.com>
2025-09-14 13:42:43 +1000

    Initial Commit!

--- Inspetor-Issue/ContentView.swift
+++ Inspetor-Issue/ContentView.swift
@@ -9,16 +9,156 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            EventSearchView()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+
+//MARK: EventSearchView
+struct EventSearchView: View {
+    @State var searchedText = ""
+    @State var showInspector = false
+    @State var presentingItemID: Int?
+    @Namespace var customNamespace
+    var body: some View {
+        Group {
+            ScrollView {
+                HStack {
+                    Spacer(minLength: 0)
+                    LazyVStack(spacing: 10) {
+                        ForEach([1, 2, 3, 4, 5, 6, 7, 8], id: \.self) { id in
+                            Button(action: {
+                                showInspector = false
+                                presentingItemID = id
+                            }, label: {
+                                Text("Lorem Ipsum Dolor Sit Amet #\(id)")
+                            })
+                            .buttonStyle(.plain)
+                            .matchedTransitionSource(id: id, in: customNamespace)
+                            .matchedGeometryEffect(id: id, in: customNamespace)
+                        }
+                    }
+                    .frame(maxWidth: 300)
+                }
+                .padding(.horizontal)
+                Spacer(minLength: 0)
+            }
+            .navigationDestination(item: $presentingItemID) { id in
+#if os(iOS)
+                EventDetailView(id: id)
+                    .navigationTransition(.zoom(sourceID: id, in: customNamespace))
+#else
+                EventDetailView(id: id)
+#endif
+            }
+        }
+        .searchable(text: $searchedText, prompt: "Placeholder")
+        .navigationTitle("Event")
+        .navigationSubtitle("Subtitle")
+        .toolbar {
+            ToolbarItem {
+                Button(action: {
+                    showInspector = true
+                }, label: {
+                    Image(systemName: "exclamationmark")
+                })
+            }
+        }
+        .onDisappear {
+            showInspector = false
+        }
+        .withSystemBackground()
+        .inspector(isPresented: $showInspector) {
+            InspectorView()
+                .presentationDetents([.medium, .large])
+                .presentationDragIndicator(.visible)
+                .presentationBackgroundInteraction(.enabled)
+        }
+        .withSystemBackground() // This modifier MUST be placed BOTH before
+                                // and after `inspector` to make it work as expected
+    }
 }
+
+
+public extension View {
+    @ViewBuilder
+    func withSystemBackground() -> some View {
+#if os(iOS)
+        self
+            .background(Color(.systemGroupedBackground))
+#else
+        self
+#endif
+    }
+}
+
+
+struct EventDetailView: View {
+    var id: Int
+    var body: some View {
+        Text("\(id)")
+    }
+}
+
+struct InspectorView: View {
+    let filterKeysOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    var body: some View {
+        Form {
+            Section(content: {
+                ForEach(filterKeysOrder, id: \.self) { key in
+                   Text("\(key)")
+                }
+            })
+        }
+    }
+}
+
+
+// This is a weird method that can sometimes fix some issues happening on SwiftUI.
+// Would this be helpful?
+
+#if os(macOS)
+/// Hi, what happened?
+/// We NEED this to workaround a bug (maybe of SwiftUI?)
+struct HereTheWorld<each T, V: View>: NSViewRepresentable {
+    private var controller: NSViewController
+    private var viewBuilder: (repeat each T) -> V
+    init(arguments: (repeat each T) = (), @ViewBuilder view: @escaping (repeat each T) -> V) {
+        self.controller = NSHostingController(rootView: view(repeat each arguments))
+        self.viewBuilder = view
+    }
+    func makeNSView(context: Context) -> some NSView {
+        self.controller.view
+    }
+    func updateNSView(_ nsView: NSViewType, context: Context) {}
+    func updateArguments(_ arguments: (repeat each T)) {
+        let newView = viewBuilder(repeat each arguments)
+        controller.view = NSHostingView(rootView: newView)
+    }
+}
+#else
+/// Hi, what happened?
+/// We NEED this to workaround a bug (maybe of SwiftUI?)
+struct HereTheWorld<each T, V: View>: UIViewRepresentable {
+    private var controller: UIViewController
+    private var viewBuilder: (repeat each T) -> V
+    init(arguments: (repeat each T) = (), @ViewBuilder view: @escaping (repeat each T) -> V) {
+        self.controller = UIHostingController(rootView: view(repeat each arguments))
+        self.viewBuilder = view
+    }
+    func makeUIView(context: Context) -> some UIView {
+        self.controller.view
+    }
+    func updateUIView(_ nsView: UIViewType, context: Context) {}
+    func updateArguments(_ arguments: (repeat each T)) {
+        let newView = viewBuilder(repeat each arguments)
+        let newUIViewController = UIHostingController(rootView: newView)
+        let newUIView = newUIViewController.view
+        newUIViewController.view = nil // detach
+        controller.view = newUIView
+    }
+}
+#endif
