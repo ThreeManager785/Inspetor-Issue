a866047d0a523841f193fd80a4a89aec557ff5b9
ThreeManager785 <leimeichun50@gmail.com>
2025-09-14 13:11:34 +1000

    Initial Commit

---
+++ Inspetor-Issue/Inspetor_IssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inspetor_IssueApp.swift
+//  Inspetor-Issue
+//
+//  Created by ThreeManager785 on 2025/9/14.
+//
+
+import SwiftUI
+
+@main
+struct Inspetor_IssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
